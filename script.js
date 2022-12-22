5969ca28b8345b224625819a11b9691dda0c49ad
Muhammad Dedishov <moxmad99@mail.ru>
2022-12-22 23:12:34 +0300

    Выполнено

--- script.js
+++ script.js
@@ -1,36 +1,79 @@
 'use strict';
 
 
-const DomElement = function (selector, height, width, bg, fontSize) {
-    this.selector = selector,
-        this.height = height,
-        this.width = width,
-        this.bg = bg,
-        this.fontSize = fontSize
-    this.myFunc = function () {
-        let element;
-        if (this.selector[0] === '.') {
+document.addEventListener('DOMContentLoaded', function (event) {
+    const px = 10;
 
-            element = document.createElement('div');
-            element.className = this.selector.slice(1);
+    const DomElement = function (selector = '.block', styleAtr = {}) {
+        this.selector = selector === 'string' ? selector : (() => {
+            styleAtr = typeof selector === 'object' ? selector : {};
+            return '.block';
+        })();
+        this.height = '100px';
+        this.width = '100px';
+        this.bg = 'green';
+        this.fontSize = '10px';
+        this.cssText = '';
 
-        } else if (this.selector[0] === '#') {
-
-            element = document.createElement('p');
-            element.id = this.selector.slice(1);
-
+        if (!('height' in styleAtr)) {
+            styleAtr.height = '100px';
         }
-        element.style.cssText = `height: ${this.height}px;
-            width: ${this.width}px;
-            background: ${this.bg};
-            font-size: ${this.fontSize}px;`
+        if (!('width' in styleAtr)) {
+            styleAtr.width = '100px';
+        }
+        if (!('background' in styleAtr)) {
+            styleAtr.background = 'green';
+        }
+        if (!('font-size' in styleAtr)) {
+            styleAtr['font-size'] = '10px';
+        }
 
-        return element;
+        for (let key in styleAtr) {
+            this.cssText += `${key}: ${styleAtr[key]}; `;
+        }
     };
-};
 
-let newDiv = new DomElement('.block', 100, 150, 'red', 15);
-let newParagraph = new DomElement('#paragraph', 200, 150, 'green', 10);
+    DomElement.prototype.newElem = function () {
+        let elem;
 
-document.body.appendChild(newDiv.myFunc());
-document.body.appendChild(newParagraph.myFunc());
+        if (this.selector[0] === '.') {
+            elem = document.createElement('div');
+            elem.className = this.selector.slice(1);
+        }
+
+        if (this.selector[0] === '#') {
+            elem = document.createElement('p');
+            elem.id = this.selector.slice(1);
+        }
+
+        elem.style.cssText = this.cssText;
+        elem.style.cssText += `left: ${Math.round(window.innerWidth / 2 - parseInt(this.width) / 2)}px; `;
+        elem.style.cssText += `top: ${Math.round(window.innerHeight / 2 - parseInt(this.height) / 2)}px; `;
+
+        elem.addEventListener('click', this.transfer);
+
+        return elem;
+    };
+
+    let square = new DomElement({
+        position: 'absolute'
+    });
+
+    document.body.appendChild(square.newElem());
+
+    document.addEventListener('keydown', (event) => {
+        const div = document.querySelector('div');
+        if (event.key === 'ArrowUp') {
+            div.style.top = parseInt(div.style.top) - px + 'px';
+        }
+        if (event.key === 'ArrowRight') {
+            div.style.left = parseInt(div.style.left) + px + 'px';
+        }
+        if (event.key === 'ArrowLeft') {
+            div.style.left = parseInt(div.style.left) - px + 'px';
+        }
+        if (event.key === 'ArrowDown') {
+            div.style.top = parseInt(div.style.top) + px + 'px';
+        }
+    });
+});
